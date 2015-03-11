46ce938c5c6ee9238350c2b95474cdb185e78c96
Russell Hueske <hueske.russ690@gmail.com>
2015-03-11 12:42:58 -0500

    maybe it will work now...

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -136,6 +136,7 @@ class APIController extends AppController
             }
         }
         
+        
         Log::write('debug', 'GITHUB HOOK ACTIVATED');
         Log::write('debug', $this->request->data);
         $blah = ['blah'];
