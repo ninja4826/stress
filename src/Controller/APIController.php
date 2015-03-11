1a9c9fdd160e0a68824991405c956107b5a7df73
Russell Hueske <hueske.russ690@gmail.com>
2015-03-11 13:29:37 -0500

    ugh

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -135,7 +135,7 @@ class APIController extends AppController
                 shell_exec('cd ..');
                 Log::write('debug', shell_exec('ls -l'));
                 Log::write('debug', get_current_user());
-                Log::write('debug', $_SERVER['LOGNAME']);
+                Log::write('debug', shell_exec('ca test'));
                 Log::write('debug', exec('git -C .. pull'));
                 
                 Log::write('debug', 'PULLING');
